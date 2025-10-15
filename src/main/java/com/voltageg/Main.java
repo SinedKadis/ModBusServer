255cd8aa1d469d1592173b68623aab2d8ca09d79
SinedKadis <sidbov@gmail.com>
2025-10-15 17:57:01 +0300

    WIP server

--- src/main/java/com/voltageg/Main.java
+++ src/main/java/com/voltageg/Main.java
@@ -1,25 +1,31 @@
 package com.voltageg;
 
 
-import com.digitalpetri.modbus.client.ModbusClient;
 import com.digitalpetri.modbus.client.ModbusTcpClient;
 import com.digitalpetri.modbus.exceptions.ModbusExecutionException;
 import com.digitalpetri.modbus.exceptions.ModbusResponseException;
 import com.digitalpetri.modbus.exceptions.ModbusTimeoutException;
+import com.digitalpetri.modbus.exceptions.UnknownUnitIdException;
 import com.digitalpetri.modbus.pdu.ReadHoldingRegistersRequest;
 import com.digitalpetri.modbus.pdu.ReadHoldingRegistersResponse;
 import com.digitalpetri.modbus.pdu.WriteMultipleRegistersRequest;
+import com.digitalpetri.modbus.pdu.WriteMultipleRegistersResponse;
+import com.digitalpetri.modbus.server.*;
 import com.digitalpetri.modbus.tcp.client.NettyTcpClientTransport;
+import com.digitalpetri.modbus.tcp.server.NettyTcpServerTransport;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.Optional;
 import java.util.function.Supplier;
 
 public class Main {
     static boolean stop = false;
-    private static ModbusClient client;
+    private static ModbusServer server;
     static final Supplier<Long> tick = System::currentTimeMillis;
     static final Logger LOGGER = LoggerFactory.getLogger(Main.class);
+    static ProcessImage processImage;
+    static ReadWriteModbusServices modbusServices;
     private static final int tickRate = 500;
 
     public static void main(String[] args) {
@@ -34,52 +40,56 @@ public class Main {
 
 
     private static void init() {
-        var transport = NettyTcpClientTransport.create(cfg -> {
-            cfg.setHostname("DESKTOP-UU22OM8");
+        var transport = NettyTcpServerTransport.create(cfg -> {
+            cfg.setBindAddress("DESKTOP-UU22OM8");
             cfg.setPort(502);
         });
-
-        client = ModbusTcpClient.create(transport);
+        processImage = new ProcessImage();
+        modbusServices = new ReadWriteModbusServices() {
+            @Override
+            protected Optional<ProcessImage> getProcessImage(int unitId) {
+                return Optional.of(processImage);
+            }
+        };
+        server = ModbusTcpServer.create(transport, modbusServices);
         try {
-            client.connect();
-        } catch (ModbusExecutionException e) {
+            server.start();
+        } catch (Exception e) {
             throw new RuntimeException(e);
         }
     }
 
     private static void mainLoop() {
+        int[] registers = readRegisters(5);
+        for (int i : registers) {
+            i++;
+            if (i>65535) i = 0;
+        }
+        writeRegistries(registers);
+
+    }
+
+    private static WriteMultipleRegistersResponse writeRegistries(int[] data) {
+        return modbusServices
+                .writeMultipleRegisters(null, 1, new WriteMultipleRegistersRequest(0, 5));
+    }
+
+    private static int[] readRegisters(int quantity) {
         ReadHoldingRegistersResponse response;
         try {
-            response = client.readHoldingRegisters(
-                    1,
-                    new ReadHoldingRegistersRequest(0, 5)
-            );
-        } catch (ModbusExecutionException | ModbusResponseException | ModbusTimeoutException e) {
+            response = modbusServices.readHoldingRegisters(null, 1, new ReadHoldingRegistersRequest(0, quantity));
+        } catch (UnknownUnitIdException e) {
             throw new RuntimeException(e);
         }
         byte[] registers = response.registers();
-        StringBuilder string = new StringBuilder("Bytes: ");
-        for (int i = 0; i < registers.length; i=i+2) {
+        int[] toReturn = new int[quantity];
+        for (int i = 0,i1 = 0; i < registers.length; i=i+2,i1++) {
             int b = registers[i];
             int b1 = registers[i+1];
             if (b<0) b = 256+b;
             if (b1<0) b1 = 256+b1;
-            string.append((b<<8)+b1).append(", ");
-
-//            if (b<9) string.append(0);
-//            if (b<99) string.append(0);
-//            string.append(b).append(":");
-//            if (b1<9) string.append(0);
-//            if (b1<99) string.append(0);
-//            string.append(b1).append(", ");
+            toReturn[i1] = (b<<8)+b1;
         }
-        LOGGER.info(string.toString());
-        try {
-            client.writeMultipleRegisters(
-                    2,
-                    new WriteMultipleRegistersRequest(2, 5, new byte[]{1, 2, 22, -123, 23, 0, 0, 0, 0, 0}));
-        } catch (ModbusExecutionException | ModbusResponseException | ModbusTimeoutException e) {
-            throw new RuntimeException(e);
-        }
+        return toReturn;
     }
 }
